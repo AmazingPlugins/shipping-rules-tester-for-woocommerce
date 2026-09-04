fa9ddb7a57c44bf32e684cfa98991b30a4e85a3b
Harun R. Rayhan <harun.b13@gmail.com>
2026-09-04 21:30:05 +0600

    chore: add static analysis and CI gates

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHPStan bootstrap declarations.
+ *
+ * @package ShippingRulesTester
+ */
+
+defined( 'SRT_PLUGIN_FILE' ) || define( 'SRT_PLUGIN_FILE', __DIR__ . '/shipping-rules-tester-for-woocommerce.php' );
+defined( 'SRT_PLUGIN_DIR' ) || define( 'SRT_PLUGIN_DIR', __DIR__ . '/' );
+defined( 'SRT_PLUGIN_URL' ) || define( 'SRT_PLUGIN_URL', 'https://example.com/' );
